0a27d496892f740b2dfd3f5614bf107aae878ef8
Dawood679 <dawoodalam057@gmail.com>
2025-07-14 21:58:05 +0500

    error sloving

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -32,7 +32,7 @@ const Card = () => {
           <DialogHeader>
             <DialogTitle>Create New Todo</DialogTitle>
             <DialogDescription>
-              Don't watch the clock; do what it does. Keep going.
+              Don&apos;t watch the clock; do what it does. Keep going.
             </DialogDescription>
           </DialogHeader>
           <div className="grid gap-4">
